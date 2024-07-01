328d6e5627ecf458677dfb50a2c272125dc6a9b5
Marthijn van den Heuvel <marthijn@sidio.nl>
2024-07-01 16:13:27 +0200

    🎨 Moved code to partial classes

--- src/Sidio.MailBluster.Tests/MailBlusterClientTests.cs
+++ src/Sidio.MailBluster.Tests/MailBlusterClientTests.cs
@@ -8,7 +8,7 @@ using Sidio.MailBluster.Requests.Leads;
 
 namespace Sidio.MailBluster.Tests;
 
-public sealed class MailBlusterClientTests : IDisposable
+public sealed partial class MailBlusterClientTests : IDisposable
 {
     private readonly HttpTest _httpTest = new ();
 
@@ -20,116 +20,6 @@ public sealed class MailBlusterClientTests : IDisposable
     public void Dispose()
     {
         _httpTest.Dispose();
-    }
-
-    [Fact]
-    public async Task CreateLeadAsync_WhenRequestIsValid_ShouldReturnLead()
-    {
-        // arrange
-        _httpTest.RespondWith(ReadJsonData("CreateResponse.json", "Leads"));
-
-        var fields = new Fields();
-        fields.AddField(_fixture.Create<string>(), _fixture.Create<string>());
-
-        var request = _fixture.Build<CreateLeadRequest>().With(x => x.Fields, fields).Create();
-        var client = CreateClient();
-
-        // act
-        var response = await client.CreateLeadAsync(request);
-
-        // assert
-        response.Should().NotBeNull();
-        response.Message.Should().Be("Lead added");
-        response.Lead!.Email.Should().Be("richard@example.com");
-        response.Lead.Id.Should().Be(329395);
-        _httpTest.ShouldHaveCalled($"*/leads").WithHeader("Authorization", _options.Value.ApiKey).WithContentType("application/json").WithRequestJson(request);
-    }
-
-    [Fact]
-    public async Task UpdateLeadAsync_WhenLeadExists_ShouldReturnLead()
-    {
-        // arrange
-        _httpTest.RespondWith(ReadJsonData("UpdateResponse.json", "Leads"));
-        const string RequestEmail = "noreply@sidio.nl";
-        const string RequestMd5 = "949c658fa59ccb5a816400a4b0ad36f8";
-        var request = _fixture.Build<UpdateLeadRequest>()
-            .With(x => x.Email, RequestEmail).Create();
-        var client = CreateClient();
-
-        // act
-        var response = await client.UpdateLeadAsync(RequestEmail, request);
-
-        // assert
-        response.Should().NotBeNull();
-        response.Message.Should().Be("Lead updated");
-        response.Lead!.Email.Should().Be("richard@example.com");
-        response.Lead.Id.Should().Be(329395);
-        _httpTest.ShouldHaveCalled($"*/leads/{RequestMd5}").WithHeader("Authorization", _options.Value.ApiKey).WithContentType("application/json").WithRequestJson(request);
-    }
-
-    [Fact]
-    public async Task GetLeadAsync_WhenRequestIsValid_ShouldReturnLead()
-    {
-        // arrange
-        _httpTest.RespondWith(ReadJsonData("ReadResponse.json", "Leads"));
-        const string RequestEmail = "noreply@sidio.nl";
-        const string RequestMd5 = "949c658fa59ccb5a816400a4b0ad36f8";
-        var client = CreateClient();
-
-        // act
-        var response = await client.GetLeadAsync(RequestEmail);
-
-        // assert
-        response.Should().NotBeNull();
-        response!.Email.Should().Be("richard@example.com");
-        response.FirstName.Should().Be("Richard");
-        response.LastName.Should().Be("Hendricks");
-        response.FullName.Should().Be("Richard Hendricks");
-        response.Timezone.Should().Be("America/Los_Angeles");
-        response.IpAddress.Should().Be("162.213.1.246");
-        response.Subscribed.Should().Be(false);
-        response.OptInStatus.Should().Be("waiting");
-        response.CreatedAt.Should().Be("2016-07-23T08:03:18.954Z");
-        response.UpdatedAt.Should().Be("2016-07-23T08:03:18.954Z");
-        response.Fields.Should().BeEquivalentTo(new Dictionary<string, string> {{"gender", "Male"}, {"address", "Silicon Valley"}});
-        response.Tags.Should().BeEquivalentTo(["iPhone User", "Startup"]);
-        _httpTest.ShouldHaveCalled($"*/leads/{RequestMd5}").WithHeader("Authorization", _options.Value.ApiKey);
-    }
-
-    [Fact]
-    public async Task GetLeadAsync_WhenLeadDoesNotExist_ShouldReturnNull()
-    {
-        // arrange
-        _httpTest.RespondWith(status: 404);
-        const string RequestEmail = "noreply@sidio.nl";
-        const string RequestMd5 = "949c658fa59ccb5a816400a4b0ad36f8";
-        var client = CreateClient();
-
-        // act
-        var response = await client.GetLeadAsync(RequestEmail);
-
-        // assert
-        response.Should().BeNull();
-        _httpTest.ShouldHaveCalled($"*/leads/{RequestMd5}").WithHeader("Authorization", _options.Value.ApiKey);
-    }
-
-    [Fact]
-    public async Task DeleteLeadAsync_WhenRequestIsValid_ShouldReturnSuccess()
-    {
-        // arrange
-        const string RequestEmail = "noreply@sidio.nl";
-        const string RequestMd5 = "949c658fa59ccb5a816400a4b0ad36f8";
-        _httpTest.RespondWith(ReadJsonData("DeleteResponse.json", "Leads"));
-        var client = CreateClient();
-
-        // act
-        var response = await client.DeleteLeadAsync(RequestEmail);
-
-        // assert
-        response.Should().NotBeNull();
-        response.LeadHash.Should().Be("5a91f0b2d2c1e5c3229d906d978b7337");
-        response.Message.Should().Be("Lead deleted");
-        _httpTest.ShouldHaveCalled($"*/leads/{RequestMd5}").WithHeader("Authorization", _options.Value.ApiKey);
     }
 
     private MailBlusterClient CreateClient() => new(new FlurlClientCache(), _options, NullLogger<MailBlusterClient>.Instance);
---
+++ src/Sidio.MailBluster/MailBlusterClient.Lead.cs
@@ -0,0 +1,101 @@
+﻿using System.Net;
+using System.Text.Json;
+using Flurl.Http;
+using Microsoft.Extensions.Logging;
+using Sidio.MailBluster.Requests.Leads;
+using Sidio.MailBluster.Responses.Leads;
+
+namespace Sidio.MailBluster;
+
+public sealed partial class MailBlusterClient
+{
+    /// <inheritdoc />
+    public async Task<CreateLeadResponse> CreateLeadAsync(CreateLeadRequest request, CancellationToken cancellationToken = default)
+    {
+        if (_logger.IsEnabled(LogLevel.Trace))
+        {
+            _logger.LogTrace("Creating lead {Request}", JsonSerializer.Serialize(request));
+        }
+        else if (_logger.IsEnabled(LogLevel.Debug))
+        {
+            _logger.LogDebug("Creating lead with Email `{Email}`", request.Email.ObfuscateEmailAddress());
+        }
+
+        var response = await DefaultClient
+            .Request(MailBlusterApiConstants.Leads)
+            .PostJsonAsync(request, cancellationToken: cancellationToken)
+            .ConfigureAwait(false);
+        var result = await response.GetJsonAsync<CreateLeadResponse>().ConfigureAwait(false);
+        return result;
+    }
+
+    /// <inheritdoc />
+    public async Task<GetLeadResponse?> GetLeadAsync(string email, CancellationToken cancellationToken = default)
+    {
+        if (_logger.IsEnabled(LogLevel.Trace))
+        {
+            _logger.LogTrace("Get lead `{Email}`", email);
+        }
+        else if (_logger.IsEnabled(LogLevel.Debug))
+        {
+            _logger.LogDebug("Get lead with Email `{Email}`", email.ObfuscateEmailAddress());
+        }
+
+        var response = await DefaultClient
+            .AllowHttpStatus((int)HttpStatusCode.NotFound)
+            .Request(MailBlusterApiConstants.Leads, CreateMd5Hash(email))
+            .GetAsync(cancellationToken: cancellationToken).ConfigureAwait(false);
+
+        if (response.StatusCode < 300)
+        {
+            return await response.GetJsonAsync<GetLeadResponse>().ConfigureAwait(false);
+        }
+
+        if (response.StatusCode != 404)
+        {
+            _logger.LogDebug("Response status code {StatusCode} for get lead `{Email}`", response.StatusCode, email.ObfuscateEmailAddress());
+        }
+
+        return null;
+    }
+
+    /// <inheritdoc />
+    public async Task<UpdateLeadResponse> UpdateLeadAsync(string email, UpdateLeadRequest request, CancellationToken cancellationToken = default)
+    {
+        if (_logger.IsEnabled(LogLevel.Trace))
+        {
+            _logger.LogTrace("Updating lead {Request}", JsonSerializer.Serialize(request));
+        }
+        else if (_logger.IsEnabled(LogLevel.Debug))
+        {
+            _logger.LogDebug("Updating lead with Email `{Email}`", email.ObfuscateEmailAddress());
+        }
+
+        var response = await DefaultClient
+            .Request(MailBlusterApiConstants.Leads, CreateMd5Hash(email))
+            .PutJsonAsync(request, cancellationToken: cancellationToken)
+            .ConfigureAwait(false);
+        var result = await response.GetJsonAsync<UpdateLeadResponse>().ConfigureAwait(false);
+        return result;
+    }
+
+    /// <inheritdoc />
+    public async Task<DeleteLeadResponse> DeleteLeadAsync(string email, CancellationToken cancellationToken = default)
+    {
+        if (_logger.IsEnabled(LogLevel.Trace))
+        {
+            _logger.LogTrace("Delete lead `{Email}`", email);
+        }
+        else if (_logger.IsEnabled(LogLevel.Debug))
+        {
+            _logger.LogDebug("Delete lead with Email `{Email}`", email.ObfuscateEmailAddress());
+        }
+
+        var md5 = CreateMd5Hash(email);
+        var response = await DefaultClient
+            .Request(MailBlusterApiConstants.Leads, md5)
+            .DeleteAsync(cancellationToken: cancellationToken).ConfigureAwait(false);
+        var result = await response.GetJsonAsync<DeleteLeadResponse>().ConfigureAwait(false);
+        return result;
+    }
+}
