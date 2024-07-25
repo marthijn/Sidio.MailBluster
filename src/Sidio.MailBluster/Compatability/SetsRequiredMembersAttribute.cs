d0aa0f9f06f45a011a05ef8b1b0211785736da93
Marthijn van den Heuvel <marthijn@sidio.nl>
2024-07-25 10:29:57 +0200

    🚨 Fixed warning

--- src/Sidio.MailBluster/Compatability/SetsRequiredMembersAttribute.cs
+++ src/Sidio.MailBluster/Compatability/SetsRequiredMembersAttribute.cs
@@ -1,5 +1,6 @@
 ﻿#if !NET7_0_OR_GREATER
 
+// ReSharper disable once CheckNamespace
 namespace System.Diagnostics.CodeAnalysis;
 
 [AttributeUsage(AttributeTargets.Constructor, AllowMultiple = false, Inherited = false)]
